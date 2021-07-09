f5310c3bd6e15f050e99573e28b18b379ea53280
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2021-07-09 11:28:16 +0200

    Modifications pour initiliser quelques tests

--- src/test/java/com/ouertani/login/SpringSecurityAuthApplicationTests.java
+++ src/test/java/com/ouertani/login/SpringSecurityAuthApplicationTests.java
@@ -1,13 +1,25 @@
 package com.ouertani.login;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.ouertani.login.controller.LoginController;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class SpringSecurityAuthApplicationTests {
+	
+	@Autowired
+	LoginController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
